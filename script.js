27ac898ee9cbaf8fc587065917162a06f6770acb
Sonny Day <jonday317@gmail.com>
2025-06-22 19:57:36 -0500

    Ffs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = 'https://bcoottemxdthoopmaict.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJI...'; // truncated for clarity
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjb290dGVteGR0aG9vcG1haWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTEzMjksImV4cCI6MjA2NTc2NzMyOX0.CVYIdU0AHBDd00IlF5jh0HP264txAGh28LBJxDAA9Ng';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
