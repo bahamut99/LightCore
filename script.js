490d0d08565711f2921bbcecce49eadf61e590ae
Sonny Day <jonday317@gmail.com>
2025-07-03 22:41:21 -0500

    typo www supposedly

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
+// Forcing deploy at 10:39 PM
 // === PASTE YOUR SUPABASE URL AND PUBLIC ANON KEY HERE ===
 const SUPABASE_URL = 'https://bcoottemxdthoopmaict.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjb290dGVteGR0aG9vcG1haWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTEzMjksImV4cCI6MjA2NTc2NzMyOX0.CVYIdU0AHBDd00IlF5jh0HP264txAGh28LBJxDAA9Ng';
